c7a62cae1723b89b11af9f6bd71622a83618c4ce
leon.kepler <leon.kepler@bytedance.com>
2024-05-30 17:51:22 +0800

    new

--- env.sh
+++ env.sh
@@ -1,5 +1,7 @@
-pip install "unsloth[cu121] @ git+https://github.com/unslothai/unsloth.git"
 pip install polars
 pip install nvitop
 pip install bitsandbytes
 pip install accelerate
+pip install vllm
+sudo apt install -y ranger htop
+pip install "unsloth[cu121] @ git+https://github.com/unslothai/unsloth.git"
