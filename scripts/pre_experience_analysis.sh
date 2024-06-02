4b57c8a2c18a1bf1fe9fcfda4803d51e15e3da2b
leon.kepler <leon.kepler@bytedance.com>
2024-06-02 16:20:39 +0800

    new

--- scripts/pre_experience_analysis.sh
+++ scripts/pre_experience_analysis.sh
@@ -1,6 +1,6 @@
-python cherry_seletion/data_analysis.py \
+python3 cherry_seletion/data_analysis.py \
     --data_path data/alpaca_data.json \
-    --save_path alpaca_data_pre.pt \
+    --save_path /mnt/bn/data-tns-live-llm/leon/datasets/repetition/alpaca_data_pre.pt \
     --model_name_or_path yahma/llama-7b-hf \
     --max_length 512 \
     --prompt alpaca \
