0b89347fcce243defa7ba0614eacad071f119b9d
leon.kepler <leon.kepler@bytedance.com>
2024-06-17 17:10:44 +0800

    new

--- env.sh
+++ env.sh
@@ -4,6 +4,9 @@ pip install bitsandbytes
 pip install accelerate
 pip install vllm
 sudo apt install -y ranger htop
-pip install "unsloth[cu121] @ git+https://github.com/unslothai/unsloth.git"
+# pip install "unsloth[cu121] @ git+https://github.com/unslothai/unsloth.git"
+cp -r /mnt/bn/data-tns-live-llm/leon/datasets/unsloth /home/tiger/.local/lib/python3.9/site-packages/
 # pip install "unsloth[cu121-ampere] @ git+https://github.com/unslothai/unsloth.git"
 pip install torch torchvision torchaudio flash-attn
+pip install openai==0.28.0
+pip install promptsource
