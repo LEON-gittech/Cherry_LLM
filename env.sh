54afb60a6bdbb832b981b37eb8ec486904cc79d3
leon.kepler <leon.kepler@bytedance.com>
2024-07-03 15:33:19 +0800

    new

--- env.sh
+++ env.sh
@@ -5,6 +5,7 @@ pip install accelerate
 pip install vllm
 pip install datasets
 sudo apt install -y ranger htop
+pip install trl
 # pip install "unsloth[cu121] @ git+https://github.com/unslothai/unsloth.git"
 pip install "unsloth[cu121-ampere] @ git+https://github.com/unslothai/unsloth.git"
 cp -r /mnt/bn/data-tns-live-llm/leon/datasets/unsloth /home/tiger/.local/lib/python3.9/site-packages/
