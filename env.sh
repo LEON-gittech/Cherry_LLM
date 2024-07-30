6acb1d5182e2c4bf0f5b5cba2de366cffe69c9fc
leon <2194369201@qq.com>
2024-07-30 21:23:24 +0800

    new

--- env.sh
+++ env.sh
@@ -9,7 +9,6 @@ pip install "unsloth[cu121-ampere] @ git+https://github.com/unslothai/unsloth.gi
 # cp -r /mnt/bn/data-tns-live-llm/leon/datasets/unsloth /home/tiger/.local/lib/python3.9/site-packages/
 pip install torch torchvision torchaudio
 # pip install flash-attn
-pip install openai==0.28.0
 pip install promptsource
 pip install sentence-transformers
 pip install matplotlib
@@ -27,3 +26,5 @@ pip install seqeval
 pip install benepar spacy
 python3 -m spacy download en_core_web_md
 pip install --force-reinstall "xformers<0.0.27 vllm<0.5.0 numpy<2.0.0"
+pip install -U transformers peft
+pip install openai==0.28.0
