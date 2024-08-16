c5bc9b83436ad44eeb8c71836fc326e7d8907099
leon.kepler <leon.kepler@bytedance.com>
2024-08-16 23:11:24 +0800

    new

--- env.sh
+++ env.sh
@@ -7,7 +7,6 @@ pip install trl
 # pip install "unsloth[cu121] @ git+https://github.com/unslothai/unsloth.git"
 pip install "unsloth[cu121-ampere] @ git+https://github.com/unslothai/unsloth.git"
 pip install torch torchvision torchaudio
-# pip install flash-attn
 pip install promptsource
 pip install sentence-transformers
 pip install matplotlib
@@ -26,4 +25,6 @@ pip install benepar spacy
 python3 -m spacy download en_core_web_md
 pip install --force-reinstall "xformers<0.0.27 vllm<0.5.0 numpy<2.0.0"
 pip install -U transformers peft
-pip install openai==0.28.0
+pip uninstall flash-attn
+pip install vllm
+pip install openai
