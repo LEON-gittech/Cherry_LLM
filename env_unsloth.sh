0dccde03dd8d9cecb04437d1695df8d739c22991
leon.kepler <leon.kepler@bytedance.com>
2025-04-03 13:39:15 +0000

    new

--- env_unsloth.sh
+++ env_unsloth.sh
@@ -4,13 +4,21 @@
 #     pytorch cudatoolkit xformers -c pytorch -c nvidia -c xformers \
 #     -y
 
-# conda activate llm
+# conda install python=3.11 \
+#     pytorch-cuda=12.1 \
+#     pytorch cudatoolkit xformers -c pytorch -c nvidia -c xformers \
+#     -y
 
+# pip install torch torchaudio torchvision xformers
 pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
 pip install FlagEmbedding sentence-transformers nvitop 
 pip install --no-deps human-eval
+pip install colorama
+pip install openai
 pip install vllm
 pip install matplotlib
-pip install -U transformers
+# pip install -U transformers
 pip install deepspeed
 pip install seqeval
+pip install evaluate rouge_score
+# pip install torch==2.1.* torchaudio torchvision transformers FlagEmbedding
